e3a02bc46946b759779a6a869966509c3e379a01
pro-minhaj <parsonal322532@gmail.com>
2024-05-07 17:57:10 +0600

    Update Left Menu Component Navigation Item Color Theme Update

--- src/components/Shared/LeftMenu/LeftMenuNavigation.jsx
+++ src/components/Shared/LeftMenu/LeftMenuNavigation.jsx
@@ -6,12 +6,12 @@ const LeftMenuNavigation = ({ item }) => {
     const path = usePathname();
 
     return (
-        <Link className={`${path === item.path ? "dark:bg-[#3A3B3C] bg-[#F0F2F5]" : ""} block px-3 hover:bg-[#F2F2F2] dark:text-gray-300 dark:hover:bg-[#3A3B3C] rounded-md py-2 text-[1.02rem transition-all duration-300`} href={item.path}>
+        <Link className={`${path === item.path ? "dark:bg-[#3A3B3C] bg-[#F0F2F5]" : ""} block px-3 hover:bg-[#F2F2F2] dark:text-dark-text text-light-text dark:hover:bg-[#343536] rounded-md py-2 text-[1.02rem transition-all duration-300`} href={item.path}>
             <span className="flex items-center gap-2 sm:gap-3">
-                <span className={`${path === item.path ? "dark:bg-[#1877F2] !bg-[#1877F2] text-[#ffffff]" : ""} bg-gray-300 dark:bg-gray-600 rounded-full p-2`}>
+                <span className={`${path === item.path ? "dark:bg-[#1877F2] !bg-[#1877F2] text-[#ffffff]" : ""} dark:bg-dark-bg bg-light-bg rounded-full p-2.5`}>
                     {item.icon}
                 </span>
-                <span className="text-lg font-medium">
+                <span className="text-[1rem] font-medium">
                     {item.name}
                 </span>
             </span>
--- src/components/Shared/LeftMenu/ReloadPageBtn.jsx
+++ src/components/Shared/LeftMenu/ReloadPageBtn.jsx
@@ -8,12 +8,12 @@ const ReloadPageBtn = () => {
     };
 
     return (
-        <button onClick={reloadPage} className="w-full px-3 hover:bg-[#F2F2F2] dark:text-gray-300 dark:hover:bg-[#3A3B3C] rounded-md py-2 text-[1.02rem transition-all duration-300">
+        <button onClick={reloadPage} className="w-full px-3 hover:bg-[#F2F2F2] dark:text-dark-text text-light-text dark:hover:bg-[#343536] rounded-md py-2 text-[1.02rem transition-all duration-300">
             <span className="flex items-center gap-2 sm:gap-3">
-                <span className="p-2 bg-gray-300 rounded-full dark:bg-gray-600">
+                <span className="p-2 rounded-full dark:bg-dark-bg bg-light-bg">
                     <MdAutorenew className="text-2xl" />
                 </span>
-                <span className="text-lg font-medium">
+                <span className="text-[1rem] font-medium">
                     Latest Feed
                 </span>
             </span>
