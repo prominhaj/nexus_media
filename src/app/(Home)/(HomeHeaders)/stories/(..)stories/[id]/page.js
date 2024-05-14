8fc6add4e42dd0bc69ddca67ad071ccf774136db
pro-minhaj <parsonal322532@gmail.com>
2024-05-15 04:26:26 +0600

    Delete Story Details Modal Page Loading Component

--- src/app/(Home)/(HomeHeaders)/stories/(..)stories/[id]/page.js
+++ src/app/(Home)/(HomeHeaders)/stories/(..)stories/[id]/page.js
@@ -1,16 +1,12 @@
 import Modal from '@/components/Global/Modal/Modal';
 import { getSingleStory } from '@/server/story';
 import StoryDetailsCard from '../../comp/StoryDetailsCard';
-import StoryDetailsLoading from '../../comp/StoryDetailsLoading';
 
 const ModalStoriesDetailsPage = async ({ params: { id } }) => {
     const story = await getSingleStory(id);
 
     return (
         <>
-            <div className='flex items-center justify-center !w-[36rem] text-center transition-colors rounded-md shadow-xl'>
-                <StoryDetailsLoading />
-            </div>
             <Modal>
                 <StoryDetailsCard story={story} />
             </Modal>
